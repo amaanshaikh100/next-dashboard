353f8cef314bc4043f10d9815818eabd351ddedb
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-04-26 23:33:54 +0530

    added theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { ArrowRightToLine } from "lucide-react";
-import React from "react";
-
 function HomePage() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <Button variant="default" size="lg">
-        Click me
-        <ArrowRightToLine />
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default HomePage;
